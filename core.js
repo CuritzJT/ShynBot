1caa62af0ee78ed9cc667aa0124603351e873f2d
Brayden Elizier Gshnolbene <plotjointhq@gmail.com>
2015-12-07 22:29:36 -0500

    updated Core.js

--- core.js
+++ core.js
@@ -1,1 +1,15 @@
+var Discord = require("discord.js");
+var auth = require("./auth.json");
+var help = require("./help.json");
+var client = new Discord.Client();
 
+client.on("ready", function(ready){
+  bot.setUsername("Shyn");
+  bot.setStatus("online", "half life 2");
+});
+
+client.on("message", function(message){
+  
+});
+
+client.login(auth.email, auth.pass);
